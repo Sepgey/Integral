25a8dc4de245c94da33b3b15a01c22ba275aa34d
Sepgey <starberdyev@mail.ru>
2021-06-11 17:02:35 +0300

    Initial commit

---
+++ Integral/Calculation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+
+namespace Integral
+{
+    public class Calculation
+    {
+        private Func<double, double> func;
+
+        private double _leftPoint;
+
+        private double _rightPoint;
+
+        private double _eps;
+
+        private Semaphore _semaphore;
+
+        public double result { get; set; }
+        
+        private volatile uint _started;
+        private volatile uint _finished;
+
+
+        public bool HasFinished => _started == _finished;
+        public Calculation(Func<double, double> function, double startPoint, double finalPoint, double e)
+
+        {
+
+            func = function;
+
+            _semaphore = new Semaphore(4, 4);
+
+            _leftPoint = startPoint;
+
+            _rightPoint = finalPoint;
+
+            _eps = e;
+
+        }
+
+        private void Calculate(object args)
+
+        {
+
+            _semaphore.WaitOne();
+
+            var left = (double)((object[])args)[0];
+
+            var right = (double)((object[])args)[1];
+
+            var length = right - left;
+
+            var h = length / 2;
+
+            var middle = left + h;
+
+            if (length < _eps)
+
+            {
+
+                result += func(middle) * length;
+
+            }
+
+            else
+
+            {
+
+                var threadLeft = new Thread(Calculate);
+
+                threadLeft.Start(new object[] { left, middle });
+
+                var threadRight = new Thread(Calculate);
+
+                threadRight.Start(new object[] { middle, right });
+
+            }
+
+            _semaphore.Release();
+
+        }
+
+        public void StartCalculate()
+
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var thread = new Thread(Calculate);
+
+            thread.Start(new object[] { _leftPoint, _rightPoint });
+
+            watch.Stop();
+
+            var elapsedMs = watch.ElapsedMilliseconds; 
+            Console.WriteLine($"Время в многопоточном режиме: {watch.ElapsedMilliseconds}");
+
+        }
+    }
+}
---
+++ Integral/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace Integral
+{
+    class Program
+    {
+        public delegate double Function(double x);
+
+        static void Main(string[] args)
+
+        {
+            double startPoint = 1;
+
+            double finalpoint = 2;
+
+            var integral = new Calculation(f, startPoint, finalpoint, 0.001);
+
+            integral.StartCalculate();
+
+            Thread.Sleep(300);
+
+            Console.WriteLine($"Результат: {integral.result}");
+
+            int n = 1000;
+
+            double[] y = new double[n];
+
+            double h = (finalpoint - startPoint) / (n - 1);
+
+            for (int i = 0; i < n; i++)
+
+            {
+                double x = i * h;
+
+                y[i] = (5 * Math.Pow(x, 4)) + (2 * Math.Sqrt(x)) - (4 / x);
+            }
+
+            var result = Trapezoidal(f, startPoint, finalpoint, n);
+
+            Console.WriteLine("Результат: {0}", result);
+
+            Console.ReadKey();
+        }
+
+        static double f(double x)
+
+        {
+            return (5 * Math.Pow(x, 4)) + (2 * Math.Sqrt(x)) - (4 / x);
+        }
+
+        public static double Trapezoidal(Function f, double a, double b, int n)
+
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            double sum = 0;
+
+            double h = (b - a) / n;
+
+            for (int i = 0; i < n; i++)
+
+            {
+                sum += 0.5 * h * (f(a + i * h) + f(a + (i + 1) * h));
+            }
+
+            watch.Stop();
+
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Время в однопоточном режиме: " + Convert.ToSingle(elapsedMs));
+
+            return sum;
+        }
+    }
+}
